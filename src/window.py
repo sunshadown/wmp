fbb243314eacd6670ad2be63bfa168d0da6fbee4
Bindas Dawid <dawid.bindas@tieto.com>
2019-10-10 17:23:04 +0200

    fixed background

--- src/window.py
+++ src/window.py
@@ -5,6 +5,7 @@ class Window:
     window = pyglet.window.Window(caption='WMP alpha version')
     window_x = None
     window_y = None
+    background = pyglet.image.load('./resources/images/background.jpg')
     label = pyglet.text.Label('WMP alpha',
                           font_name='Times New Roman',
                           font_size=36,
@@ -14,13 +15,13 @@ class Window:
         self.window.set_fullscreen(True)
         self.window_x = self.window.width
         self.window_y = self.window.height
-        #self.window.set_caption('WMP alpha version')
+
 
         @self.window.event
         def on_draw():
             self.window.clear()
+            self.background.blit(0,0)
             self.label.draw()
-
         pyglet.app.run()
 
 
