3e95004c1f9a591c67d7f7ebd36d63d8f6fb4820
Bindzi <dawidbindis@gmail.com>
2019-10-12 01:28:56 +0200

    working button creator

--- src/button.py
+++ src/button.py
@@ -3,26 +3,36 @@ from pyglet.gl import *
 from model import Model
 
 class Button(Model):
-    def __init__(self, x, y, size_x, size_y, image):
+    def __init__(self, x, y, size_x, size_y, image, text):
         self.x = x
         self.y = y
         self.size_x = size_x
         self.size_y = size_y
         self.image = image
+        self.label = pyglet.text.Label(text,
+                          font_name='Times New Roman',
+                          font_size=18,
+                          x = x, y = y,
+                          anchor_x='center', anchor_y='center')
+        self.image.anchor_x = self.image.width // 2
+        self.image.anchor_y = self.image.height // 2
         self.image_x = image.width
         self.image_y = image.height
         self.sprite = pyglet.sprite.Sprite(img = self.image)
-        self.sprite.x = x #- size_x / 2
-        self.sprite.y = y #- size_y / 2 
-        scale_x = float(self.image_x / size_x)
-        scale_y = float(self.image_y / size_y)
-        print(scale_x)
-        print(scale_y)
+        self.sprite.x = x 
+        self.sprite.y = y
+        scale_x = 1.0
+        scale_y = 1.0
+        if size_x < self.sprite.width and size_y < self.sprite.height:
+            scale_x = float(size_x / self.sprite.width)
+            scale_y = float(size_y / self.sprite.height)
+        
         self.sprite.scale_x = scale_x
         self.sprite.scale_y = scale_y
 
     def render(self):
         self.sprite.draw()
+        self.label.draw()
     
     def update(self, dt):
         pass
@@ -30,7 +40,3 @@ class Button(Model):
     def SetPosition(self, x, y):
         self.x = x
         self.y = y
-        npos_x = x -  self.size_x // 2
-        npos_y = y - self.size_y //2
-        self.sprite.x = npos_x
-        self.sprite.y = npos_y
--- src/window.py
+++ src/window.py
@@ -1,19 +1,20 @@
 import pyglet
 from pyglet.window import key
-from button import Button
+from menu import Menu
+
 class Window:
     window = pyglet.window.Window(caption='WMP alpha version')
     window_x = None
     window_y = None
+
+    menu = Menu()
     background = pyglet.image.load('./resources/images/background.jpg')
-    button_image = pyglet.image.load('./resources/images/stypendium.png') 
-    button = Button(0, 0, 100, 100, button_image)
-    buttons = []
     label = pyglet.text.Label('WMP alpha',
                           font_name='Times New Roman',
                           font_size=36,
                           x=200, y=800,
                           anchor_x='center', anchor_y='center')
+
     def __init__(self):
         self.window.set_fullscreen(True)
         self.window_x = self.window.width
@@ -27,7 +28,7 @@ class Window:
             self.window.clear()
             self.background.blit(0,0)
             self.label.draw()
-            self.button.render()
+            self.menu.draw()
         
         @self.window.event
         def on_key_press(symbol, modifiers):
