acba718c8e07a19e9e97c7e29269f7be8e4919cd
bindzi <dawidbindis@gmail.com>
2019-12-12 02:18:49 +0100

    darknet integrate

--- src/classifier.py
+++ src/classifier.py
@@ -2,43 +2,16 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
+from darkflow.net.build import TFNet
+import cv2
 
 print("TensorFlow version: ",tf.__version__)
 
-fashion_mnist = tf.keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+options = {"model": "cfg/yolo.cfg", "train": "", "dataset": "dokumenty/learn", "annotation": "dokumenty/learn" , "labels": "dok_lab.txt"}
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-print(train_images.shape)
+tfnet = TFNet(options)
 
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-
-
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
-
-
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
-
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-model.fit(train_images, train_labels, epochs=10)
-
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-print('\nTest accuracy:', test_acc)
+imgcv = cv2.imread("./dokumenty/learn/3.jpg")
+result = tfnet.return_predict(imgcv)
+print(result)
