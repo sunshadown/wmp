1511cfd7897ff1a8125ec39e98b9aa18c7fc834b
Michał Drozd <dm39255@zut.edu.pl>
2020-01-14 14:49:21 +0100

    adding stamp and logo segmentation

--- src/classifier_predict.py
+++ src/classifier_predict.py
@@ -1,21 +1,21 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import os
 from darkflow.net.build import TFNet
 import cv2
 import json
 import cv2
+from stamp_segmentation import segmentation
 
-options = {"model": "doc_seg/cfg/tiny-yolo-voc-6c.cfg","load": -1, "gpu": 0.8, "labels": "doc_seg/dok_lab.txt",'backup':'ckpt/',"threshold": 0.1}
+options = {"model": "doc_seg/cfg/tiny-yolo-voc-6c.cfg","load": -1, "gpu": 0.8, "labels": "doc_seg/dok_lab.txt",'backup':'ckpt/',"threshold": 0.4}
 
 tfnet = TFNet(options)
 tfnet.load_from_ckpt()
 imgcv = cv2.imread("doc_seg/dokumenty/learn/38.jpg")
 result = tfnet.return_predict(imgcv)
 print(result)
-
 
 for i in range(len(result)):
     data = result[i]
@@ -25,15 +25,18 @@ for i in range(len(result)):
     if text == 'text':
         imgcv = cv2.rectangle(imgcv, (top['x'],top['y']),(bot['x'],bot['y']), (255,0,0), 2)
     if text == 'stamp':
-        imgcv = cv2.rectangle(imgcv, (top['x'],top['y']),(bot['x'],bot['y']), (0,255,0), 2)
+        imgcv = segmentation(imgcv,top,bot,(0,255,0))
+        #imgcv = cv2.rectangle(imgcv, (top['x'],top['y']),(bot['x'],bot['y']), (0,255,0), 2)
     if text == 'logo':
-        imgcv = cv2.rectangle(imgcv, (top['x'],top['y']),(bot['x'],bot['y']), (0,0,255), 2)
+        imgcv = segmentation(imgcv,top,bot,(0,0,255))
+        #imgcv = cv2.rectangle(imgcv, (top['x'],top['y']),(bot['x'],bot['y']), (0,0,255), 2)
     if text == 'foto':
         imgcv = cv2.rectangle(imgcv, (top['x'],top['y']),(bot['x'],bot['y']), (255,255,0), 2)
     if text == 'table':
         imgcv = cv2.rectangle(imgcv, (top['x'],top['y']),(bot['x'],bot['y']), (255,0,255), 2)
     if text == 'signature':
         imgcv = cv2.rectangle(imgcv, (top['x'],top['y']),(bot['x'],bot['y']), (0,255,255), 2)
+
 
 cv2.namedWindow("top",cv2.WINDOW_FREERATIO)
 cv2.imshow('top',imgcv)
---
+++ src/stamp_segmentation.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+
+
+def segmentation(img,top,bot,color):
+    crop,edged,x1,x2,y1,y2 = resizeROI(img,top,bot,5)
+    contours, hierarchy=cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    if len(contours) > 1:
+        kernel = np.ones((3,3), np.uint8)
+        dilation = cv2.dilate(edged, kernel)
+        edged = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+        contours, hierarchy=cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        if len(contours) > 1:
+            temp = largest_contour_find(contours)
+            contours = []
+            contours.append(temp)
+    cv2.drawContours(crop,contours,-1,color,cv2.FILLED)
+    img[y1:y2,x1:x2] = crop
+    return img
+
+def resizeROI(img,top,bot,tresh):
+    tresh = tresh*255
+    yT = top['y']
+    yB = bot['y']
+    xT = top['x']
+    xB = bot['x']
+    crop = img[yT:yB,xT:xB]
+    gray=cv2.cvtColor(crop,cv2.COLOR_BGR2GRAY)
+    edged=cv2.Canny(gray,30,200)
+    yImg = len(img)-1
+    xImg = len(img[0])-1
+    while 1:
+        flag = 0
+        y = len(crop)-1
+        x = len(crop[0])-1
+        if np.sum(edged[0:y,0]) > tresh:
+            xT = xT - 1
+            flag = 1
+            if xT < 0 :
+                xT = 0
+                flag = 0         
+        if np.sum(edged[0:y,x]) > tresh:
+            xB = xB + 1
+            flag = 1
+            if xB > xImg :
+                xB = xImg
+                flag = 0             
+        if np.sum(edged[0,0:x]) > tresh:
+            yT = yT - 1
+            flag = 1
+            if yT < 0 :
+                yT = 0
+                flag = 0 
+        if np.sum(edged[y,0:x]) > tresh:
+            yB = yB + 1
+            flag = 1
+            if yB > yImg :
+                yB = yImg
+                flag = 0 
+        if flag:
+            crop = img[yT:yB,xT:xB]
+            gray=cv2.cvtColor(crop,cv2.COLOR_BGR2GRAY)
+            edged=cv2.Canny(gray,30,200)
+        else:
+            break       
+    return crop,edged,xT,xB,yT,yB
+
+def largest_contour_find(contours):
+    max_area=0
+    largest_contour=-1
+    for i in range(len(contours)):
+        cont=contours[i]
+        area=cv2.contourArea(cont)
+        if(area>max_area):
+            max_area=area
+            largest_contour=i
+    if(largest_contour==-1):
+        return 0
+    else:
+        l_contour=contours[largest_contour]
+        return l_contour
